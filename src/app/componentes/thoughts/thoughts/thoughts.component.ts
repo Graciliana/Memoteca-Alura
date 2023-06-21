a3a0e050e77731cc26fe96198464df6e368f6c29
Graciliana Kascher <eagraciliana@gmail.com>
2023-06-21 16:09:36 -0300

    :sparkles: feat: adicionando a directiva NgClass para tornar os cards em tamanhos diferentes

--- src/app/componentes/thoughts/thoughts/thoughts.component.ts
+++ src/app/componentes/thoughts/thoughts/thoughts.component.ts
@@ -3,12 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-thoughts',
   templateUrl: './thoughts.component.html',
-  styleUrls: ['./thoughts.component.css']
+  styleUrls: ['./thoughts.component.css'],
 })
 export class ThoughtsComponent {
-    @Input() pensamento = {
-        conteudo: 'I love Angular',
-        autoria: 'Nay',
-        modelo: 'modelo3'
+  @Input() pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
+    modelo: 'modelo3',
+  };
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
     }
+    return 'pensamento-p';
+  }
 }
