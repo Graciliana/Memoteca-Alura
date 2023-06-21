377fcb244115034f846e33fc8b3cba41c9e95c03
Graciliana Kascher <eagraciliana@gmail.com>
2023-06-21 13:48:02 -0300

    :sparkles: feat: configurado as rodas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateThoughtsComponent } from './componentes/thoughts/create-thoughts/create-thoughts.component';
+import { ListThoughtsComponent } from './componentes/thoughts/list-thoughts/list-thoughts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'listThought',
+    pathMatch: 'full',
+  },
+  {
+    path: 'createThought',
+    component: CreateThoughtsComponent,
+  },
+  {
+    path: 'listThought',
+    component: ListThoughtsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
