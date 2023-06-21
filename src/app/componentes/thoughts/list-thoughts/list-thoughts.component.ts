70de9ca12587c68c19ea376a970f8336d59dc1b2
Graciliana Kascher <eagraciliana@gmail.com>
2023-06-21 15:24:14 -0300

    :sparkles: feat: adicionando a directiva do for proporcionando a comunicação entre os componentes

--- src/app/componentes/thoughts/list-thoughts/list-thoughts.component.ts
+++ src/app/componentes/thoughts/list-thoughts/list-thoughts.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list-thoughts',
   templateUrl: './list-thoughts.component.html',
-  styleUrls: ['./list-thoughts.component.css']
+  styleUrls: ['./list-thoughts.component.css'],
 })
 export class ListThoughtsComponent {
-  listaPensamentos = []
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input ()',
+      autoria: 'Componente Filho',
+      modelo: 'modelo1',
+    },
+  ];
 }
--- src/app/componentes/thoughts/thoughts/thoughts.component.ts
+++ src/app/componentes/thoughts/thoughts/thoughts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-thoughts',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./thoughts.component.css']
 })
 export class ThoughtsComponent {
-    pensamento = {
+    @Input() pensamento = {
         conteudo: 'I love Angular',
         autoria: 'Nay',
         modelo: 'modelo3'
