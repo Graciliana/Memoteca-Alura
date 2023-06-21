a1fd2346c83992f40d4c730fd9444634830f9166
Graciliana Kascher <eagraciliana@gmail.com>
2023-06-21 12:41:06 -0300

    :sparkles: feat: Event Binding das acoes dos botoes de salvar e cancelar

--- src/app/componentes/thoughts/create-thoughts/create-thoughts.component.ts
+++ src/app/componentes/thoughts/create-thoughts/create-thoughts.component.ts
@@ -3,13 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-thoughts',
   templateUrl: './create-thoughts.component.html',
-  styleUrls: ['./create-thoughts.component.css']
+  styleUrls: ['./create-thoughts.component.css'],
 })
 export class CreateThoughtsComponent {
   pensamento = {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
-}
+    modelo: '',
+  };
+
+  criarPensamento() {
+    alert('Novo pensamento criado!');
+  }
+  cancelar() {
+    alert('Cancelado');
+  }
 }
